c9ae4865251ade327cee370dce791ab46ff8eb35
Sarah Shimizu <slshimizu92@gmail.com>
2022-05-23 12:07:35 +0100

    Initial Push

---
+++ sendcompliment/math.py
@@ -0,0 +1,9 @@
+def math_add(a,b):
+    return a+b
+
+if __name__ == "__main__":
+    # Le Wagon location
+    a=2
+    b=2
+    result = math_add(a,b)
+    print(result)
---
+++ tests/test_math.py
@@ -0,0 +1,5 @@
+from sendcompliment.math import math_add
+
+def test_mathstuff():
+    result = math_add(5,1)
+    assert result ==6
